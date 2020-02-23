654a840cdec8e8ac562caa479ba8ba552bfc13a0
riyazpasha <riyazpasha880@gmail.com>
2020-02-24 01:17:11 +0530

    [Riyaz] Overriding enum type with other values

--- Enum-With-Values.js
+++ Enum-With-Values.js
@@ -7,4 +7,9 @@ var Color;
     Color["BLUE"] = "#00f";
 })(Color || (Color = {}));
 ;
-console.log("Color", Color);
+console.log("Color", Color); // { RED: '#f00', GREEN: '#0f0', BLUE: '#00f' }
+var favColor = Color.BLUE;
+console.log("favColor", favColor); // #00f
+console.log("typeof favColor", typeof favColor); //string
+favColor = "RED"; // Type '"RED"' is not assignable to type 'Color' but js code still runs
+console.log("favColor", favColor); // RED
--- Enum-With-Values.ts
+++ Enum-With-Values.ts
@@ -6,3 +6,10 @@ enum Color {
     BLUE = "#00f",
 };
 console.log("Color", Color); // { RED: '#f00', GREEN: '#0f0', BLUE: '#00f' }
+
+let favColor: Color = Color.BLUE;
+console.log("favColor", favColor); // #00f
+console.log("typeof favColor", typeof favColor); //string
+
+favColor = "RED" // Type '"RED"' is not assignable to type 'Color' but js code still runs
+console.log("favColor", favColor); // RED
