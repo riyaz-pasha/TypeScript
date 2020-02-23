de66249f3638ce773f48996e0822bcef183376ba
riyazpasha <riyazpasha880@gmail.com>
2020-02-24 00:37:25 +0530

    [Riyaz] assigned type of variable as null but got object in console log

--- Variable-Types.js
+++ Variable-Types.js
@@ -4,3 +4,5 @@ var isEmployee = true;
 console.log(typeof isEmployee);
 isEmployee = "No"; // in type script it'll throw an error but in js it will still run
 console.log(isEmployee);
+var nullVar = null;
+console.log("typeof nullVar", typeof nullVar);
--- Variable-Types.ts
+++ Variable-Types.ts
@@ -4,3 +4,6 @@ console.log(typeof isEmployee);
 
 isEmployee = "No"; // in type script it'll throw an error but in js it will still run
 console.log(isEmployee);
+
+let nullVar: null = null;
+console.log("typeof nullVar", typeof nullVar)
