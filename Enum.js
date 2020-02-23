3b421b674c543572094d685efb03d4e6f1a1d0d1
riyazpasha <riyazpasha880@gmail.com>
2020-02-24 00:57:16 +0530

    [Riyaz] accessing an Enum

--- Enum.js
+++ Enum.js
@@ -7,3 +7,8 @@ var Color;
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {}));
 ;
+console.log(Color); // { '0': 'Red', '1': 'Green', '2': 'Blue', Red: 0, Green: 1, Blue: 2 }
+console.log(Color[0]); // Red
+console.log(Color["0"]); // Red
+console.log(Color.Red); //0
+console.log(Color["Red"]); //0
--- Enum.ts
+++ Enum.ts
@@ -1,3 +1,9 @@
 export { }
 
 enum Color { Red, Green, Blue };
+console.log(Color);// { '0': 'Red', '1': 'Green', '2': 'Blue', Red: 0, Green: 1, Blue: 2 }
+console.log(Color[0]) // Red
+console.log(Color["0"]) // Red
+
+console.log(Color.Red) //0
+console.log(Color["Red"]) //0
