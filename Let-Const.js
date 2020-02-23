c68e6df4b4b5491dc8125317d322667db8934b54
riyazpasha <riyazpasha880@gmail.com>
2020-02-24 00:17:57 +0530

    [Riyaz] Const and Let scope

---
+++ Let-Const.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var PI = 3.14;
+var Age = 20;
+if (true) {
+    var Age_1 = 30;
+}
+if (true) {
+    var Age_2 = 40;
+}
+if (true) {
+    var PI_1 = 3.1411;
+}
+if (true) {
+    var PI_2 = 3.1422;
+}
---
+++ Let-Const.ts
@@ -0,0 +1,18 @@
+export { }
+const PI = 3.14;
+
+let Age = 20;
+
+if (true) {
+    let Age = 30;
+}
+if (true) {
+    let Age = 40;
+}
+
+if (true) {
+    const PI = 3.1411
+}
+if (true) {
+    const PI = 3.1422
+}
