a8d540e4ba84789ff68da02508ac793e0d4c2b8c
riyazpasha <riyazpasha880@gmail.com>
2020-02-24 21:17:00 +0530

    [Riyaz] Add Multiples types for a variable

---
+++ Multi-Type.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var isValid;
+isValid = true;
+isValid = 0;
+isValid = "True"; // You cann't assign other than boolean or number
---
+++ Multi-Type.ts
@@ -0,0 +1,8 @@
+export { }
+
+let isValid: boolean | number;
+isValid = true;
+isValid = 0;
+
+isValid = "True"; // You cann't assign other than boolean or number
+
